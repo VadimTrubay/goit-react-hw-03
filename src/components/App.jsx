d1e74fc1d66c91d4096c5219d78e07e7624d4602
VadimTrubay <vadnetvadnet@ukr.net>
2024-03-08 12:53:56 +0200

    start react  hw-03

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import styles from './ContactForm.css';
+
+const ContactForm = () => {
+  return (
+    <div className={styles.contact_form}>
+      
+    </div>
+  )
+}
+
+export default ContactForm
