f21eca7e0563f16f1ee2c7c74caa3692c48de266
VadimTrubay <vadnetvadnet@ukr.net>
2024-03-12 18:26:49 +0200

    refactor

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import Contact from "./Contact/Contact";
+import Contact from "../../components/Contact/Contact";
 import styles from "./ContactList.module.css";
 
 const ContactList = ({ contacts, onDelete }) => {
